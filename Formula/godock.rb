9c95bf67cacdcb85daab1fc5a505a36f9f32bfa5
Jovica Zoric <zoricjovica@gmail.com>
2020-03-13 17:12:19 +0100

    Release v0.0.3

--- Formula/godock.rb
+++ Formula/godock.rb
@@ -1,9 +1,9 @@
 class Godock < Formula
   desc "The godock command lets you delete docker images and containers interactively"
   homepage "https://github.com/jzoric/godock"
-  url "https://github.com/jzoric/godock/releases/download/v0.0.2/godock_0.0.2_Darwin_amd64.tar.gz"
-  version "0.0.2"
-  sha256 "4fcfaf298e910c175012d0543e6ae50e71632fce8d2c4ab327ad780936b9913a"
+  url "https://github.com/jzoric/godock/releases/download/v0.0.3/godock_0.0.3_Darwin_amd64.tar.gz"
+  version "0.0.3"
+  sha256 "91c101522393dd0406e306012de8a53ba06a680d35cf5c0ad02f5721a2b6a859"
 
   def install
     bin.install "godock"
